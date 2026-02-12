9f04d939f5f24674310eef2ee0357358d271a30d
Avaneesh Dubey <avaneesh.d@devstree.in>
2026-02-12 16:26:13 +0530

    Added Modes Menu, and every mode's sprite, made changes in StartGame Script, PlayerHealth Script

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -69,7 +69,6 @@ public class PlayerHealth : MonoBehaviour
         if (healthFill != null)
         {
             healthFill.fillAmount = currentHealth / maxHealth;
-            lowHealth.SetActive(false);
         }
     }
 
--- Assets/Scripts/StartGame.cs
+++ Assets/Scripts/StartGame.cs
@@ -7,11 +7,18 @@ public class StartGame : MonoBehaviour
     public GameObject btnPanel;
     public GameObject gameTitle;
     public GameObject upsideDownTitle;
+    public GameObject modesPanel;
 
     public void StartButton()
     { 
         btnPanel.SetActive(false);
         gameTitle.SetActive(false);
+        modesPanel.SetActive(true);
+    }
+
+    public void OnDemoHuntClick()
+    {
+        modesPanel.SetActive(false);
         upsideDownTitle.SetActive(true);
         upsideDownGate.SetActive(true);
     }
